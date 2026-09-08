dcfecf2cf6f50f02b91f7d50513bda39de5c4296
zamoranomartina07 <zamoranomartina07@gmail.com>
2026-09-08 13:06:11 -0300

    E1: catalogo a mano y CLI ajustado segun consigna

---
+++ src/dominio/catalogo.py
@@ -0,0 +1,28 @@
+# src/dominio/catalogo.py
+
+CANCIONES = [
+    {
+        "id": "1",
+        "titulo": "De Musica Ligera",
+        "artista": "Soda Stereo",
+        "album": "Cancion Animal",
+        "genero": "Rock",
+        "duracion_seg": "213",
+    },
+    {
+        "id": "2",
+        "titulo": "Persiana Americana",
+        "artista": "Soda Stereo",
+        "album": "Signos",
+        "genero": "Rock",
+        "duracion_seg": "263",
+    },
+    {
+        "id": "3",
+        "titulo": "Crimen",
+        "artista": "Gustavo Cerati",
+        "album": "Ahi vamos",
+        "genero": "Rock",
+        "duracion_seg": "239",
+    },
+]

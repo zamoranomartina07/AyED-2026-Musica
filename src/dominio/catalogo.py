f1ca5c75b2a6d98ca51a251d0d2cec56341d1fdf
andrealescano200226-source <andrealescano200226@gmail.com>
2026-09-17 09:20:55 -0300

    Update catalogo.py

--- src/dominio/catalogo.py
+++ src/dominio/catalogo.py
@@ -25,4 +25,30 @@ CANCIONES = [
         "genero": "Rock",
         "duracion_seg": "239",
     },
+
+    {
+        "id": "4",
+        "titulo": "De Musica Ligera (cover)",
+        "artista": "Banda Tributo",
+        "album": "Tributo a Soda",
+        "genero": "Rock",
+        "duracion_seg": "220",
+    },
+    {
+        "id": "5",
+        "titulo": "De Musica Ligera (cover, en vivo)",
+        "artista": "Banda Tributo",
+        "album": "En Vivo",
+        "genero": "Rock",
+        "duracion_seg": "230",
+    },
+    {
+        "id": "6",
+        "titulo": "Persiana Americana (remix)",
+        "artista": "DJ Remixer",
+        "album": "Remixes 2020",
+        "genero": "Electrónica",
+        "duracion_seg": "200",
+    },
+    
 ]
